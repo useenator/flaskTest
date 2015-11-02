d4e9b7e5e2f3a86ca12a0fe793debfbbd6c9c371
useenator <useenator@gmail.com>
2015-11-02 02:40:22 +0000

    login done

--- FlaskWebTest/FlaskWebTest/views.py
+++ FlaskWebTest/FlaskWebTest/views.py
@@ -11,6 +11,30 @@ import models
 
 from app import app,db
 
+from flask.ext.login import LoginManager
+from flask.ext.login import login_user, logout_user, current_user, login_required
+
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+'''
+@app.before_request
+def before_request():
+    g.user = current_user
+'''
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@lm.user_loader
+def load_user(id):
+    return models.User.query.get(int(id))
+
+@app.before_request
+def before_request():
+    g.user = current_user
+
 @app.route('/')
 @app.route("/index/",methods=['GET','POST'])
 def index():
@@ -26,27 +50,27 @@ def index():
 
 @app.route('/login', methods=('GET', 'POST'))
 def login():
-    form = forms.LoginForm()
     users=models.User.query.all()
+    if g.user is not None and g.user.is_authenticated:
+        return redirect(url_for('index'))
+    form = forms.LoginForm()
     if form.validate_on_submit():
-        flash("Yay, you registered!", "success")
-        user=models.User(
-            username=form.username.data,
-            email=form.email.data,
-            password=form.password.data,
-            joined_at=datetime.datetime.utcnow()
-        )
-        try:
-            db.session.add(user)
-            db.session.commit()
-            flash("Yay, you registered!", "success")
-        except Exception as e:
-            raise
+        if models.User.query.filter(models.User.username==form.username.data,
+                                    models.User.password==form.password.data).all():
 
-        return redirect(url_for('register'))
+            userLoged=models.User.query.filter(models.User.username==form.username.data,
+                                        models.User.password==form.password.data).one()
+            # Login and validate the user.
+            # user should be an instance of your `User` class
+            login_user(userLoged)
+            flash('Logged in successfully.', "success")
+
+        return redirect(url_for('index'))
     return render_template('login.html',
                            title='Login',
                            form=form,users=users)
+
+
 
 @app.route('/register', methods=('GET', 'POST'))
 def register():
